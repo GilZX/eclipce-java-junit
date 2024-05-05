396b6875b491d3d02ed1abc5370871a6d7787165
GilZX <gilrz0101@gmail.com>
2024-05-05 16:36:15 -0600

    firtscommit

---
+++ src/main/java/com/ues/rz18015/com/ues/rz18015/App.java
@@ -0,0 +1,22 @@
+package com.ues.rz18015.com.ues.rz18015;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public String hello(){
+		return "Hello Wordl";
+	}
+	
+    public static void main( String[] args )
+    {
+    	
+    	
+    	App hw=new App();
+        System.out.println(hw.hello());
+    }
+}
